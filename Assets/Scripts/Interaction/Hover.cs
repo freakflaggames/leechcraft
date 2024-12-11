6f0abb2f2a8a893f2a4a9b70097b94a797a48676
hannnnnnnnnnnah <hannahkey013@gmail.com>
2024-12-10 23:17:22 -0500

    12-10 outline and step fixes

--- Assets/Scripts/Interaction/Hover.cs
+++ Assets/Scripts/Interaction/Hover.cs
@@ -5,18 +5,54 @@ using UnityEngine;
 
 public class Hover : MonoBehaviour
 {
-    [SerializeField] Material baseMat;
-    [SerializeField] Material outline;
+    [SerializeField] Material baseMat, outline;
+    [SerializeField] Renderer targetObject;
+    [SerializeField] List<Renderer> targets = new List<Renderer>();
+    [SerializeField] bool Multiples = false;
+    [SerializeField] float newWeight;
+
+
+    private void Update()
+    {
+        if (Input.GetKey(KeyCode.LeftShift))
+        {
+            outline.SetFloat("_Weight", 0);
+            targetObject.material = outline;
+        }
+        else
+           targetObject.material = baseMat;
+    }
 
     private void OnMouseOver()
     {
-        GetComponent<Renderer>().material = outline;
-        Debug.Log("mouseover");
+        if(!Multiples && targetObject != null)
+        {
+            outline.SetFloat("_Weight", newWeight);
+            targetObject.material = outline;
+        }
+        else
+        {
+            foreach(var target in targets)
+            {
+                outline.SetFloat("_Weight", newWeight);
+                target.material = outline;
+            }
+        }
+
     }
 
     private void OnMouseExit()
     {
-        GetComponent<Renderer>().material = baseMat;
-        Debug.Log("mouse exit");
+        if (!Multiples && targetObject != null)
+        {
+            targetObject.material = baseMat;
+        }
+        else
+        {
+            foreach (var target in targets)
+            {
+                target.material = baseMat;
+            }
+        }
     }
 }
