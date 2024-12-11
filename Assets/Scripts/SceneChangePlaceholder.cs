7552a9d1a7b53af453b696ec041d55a12459baeb
hannnnnnnnnnnah <hannahkey013@gmail.com>
2024-12-11 00:40:20 -0500

    12-11 scene change stuff

--- Assets/Scripts/SceneChangePlaceholder.cs
+++ Assets/Scripts/SceneChangePlaceholder.cs
@@ -7,7 +7,7 @@ public class SceneChangePlaceholder : MonoBehaviour
 {
     private void OnTriggerEnter(Collider other)
     {
-        if (other.tag == "Player")
+        if (other.tag == "Player" && (int)GameManager.Instance.InkController.story.variablesState["10106I_stock"] == 5)
             SceneManager.LoadScene("VillageTest");
     }
 }
